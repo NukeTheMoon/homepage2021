9f6eec1a107b61b17ff9c6c0e4eb2d68a61bd5e0
paleglow <arcybartek@gmail.com>
2021-10-14 03:05:18 +0200

    reveal content on load

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,21 +1,24 @@
 <template>
-	<div class="layout">
+	<div class="layout" :class="{ loaded: isLoaded }">
 		<slot />
 	</div>
 </template>
 
 <script>
-import { defineComponent, onMounted } from '@vue/composition-api';
+import { defineComponent, onMounted, ref } from '@vue/composition-api';
 
 import { initFirebase } from '@/helpers/initFirebase';
 
 export default defineComponent({
 	setup() {
+		const isLoaded = ref(false);
+
 		onMounted(() => {
 			initFirebase();
+			isLoaded.value = true;
 		});
 
-		return {};
+		return { isLoaded };
 	},
 });
 </script>
@@ -26,9 +29,14 @@ export default defineComponent({
 	min-height: 100vh;
 	flex-direction: column;
 	width: 100%;
+	opacity: 0;
 
 	@include landscape {
 		flex-direction: row;
+	}
+
+	&.loaded {
+		opacity: 1;
 	}
 }
 </style>
